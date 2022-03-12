4c01687c0216f02a6e42edf9e45e14b5e037fa7c
ewanytken <ewanytken@mail.ru>
2022-03-12 12:24:31 +0300

    Final

--- src/main/java/demo/bot/Bot.java
+++ src/main/java/demo/bot/Bot.java
@@ -13,15 +13,24 @@ import com.pengrad.telegrambot.request.AnswerInlineQuery;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.EditMessageText;
 
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Bot {
 
     private final Logger logger = LoggerFactory.getLogger(Bot.class);
 
     private final String PROCESSING = "Processing...";
-    TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
+    private final TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
+    private final static List<String> opponentWins = new ArrayList<String>(){{
+        add("01");
+        add("12");
+        add("20");
+    }};
 
     public void server() {
         bot.setUpdatesListener(updates -> {
@@ -42,7 +51,8 @@ public class Bot {
 
             InlineKeyboardMarkup replyMarkup = message.replyMarkup();
             InlineKeyboardButton[][] buttons = replyMarkup.inlineKeyboard();
-            String senderChose = buttons[0][0].text();
+            InlineKeyboardButton button = buttons[0][0];
+            String buttonLabel = button.text();
 
             if(replyMarkup == null){
                 return;
@@ -50,11 +60,12 @@ public class Bot {
             if(buttons == null){
                 return;
             }
-            if(!senderChose.equals(PROCESSING)){
+            if(!buttonLabel.equals(PROCESSING)){
                 return;
             }
             Long chatId = message.chat().id();
             String senderName = message.from().firstName();
+            String senderChose = button.callbackData();
             Integer messageId = message.messageId();
 
             request = new EditMessageText(chatId, messageId, message.text())
@@ -86,13 +97,40 @@ public class Bot {
 
             request = new AnswerInlineQuery(inlineQuery.id(), stone, scissors, paper).cacheTime(1);
 
-            if (callbackQuery == null) {
-                logger.debug("callbackQuery is null");
-            }
+
         }/* else if(message != null){
             long chatId = message.chat().id();
             request = new SendMessage(chatId, "I am only your bot");
         }*/
+        else if (callbackQuery != null) {
+            String[] data = callbackQuery.data().split(" ");
+
+            Long chatId = Long.parseLong(data[0]);
+            String senderName = data[1];
+            String senderChose = data[2];
+            String opponentChose = data[3];
+            String opponentName = callbackQuery.from().firstName() + "***";
+
+            if(senderChose.equals(opponentChose)){
+                request = new SendMessage(chatId, "Nobody wins =((");
+            } else if(opponentWins.contains(senderChose + opponentChose)){
+                request = new SendMessage(
+                        chatId, String.format(
+                                        "%s (%s) was beaten by %s (%s)",
+                                        senderName, senderChose,
+                                        opponentName, opponentChose
+                )
+                );
+            } else {
+                request = new SendMessage(
+                        chatId, String.format(
+                        "%s (%s) was beaten by %s (%s)",
+                        opponentName, opponentChose,
+                        senderName, senderChose
+                )
+                );
+            }
+        }
         if(request != null){
             bot.execute(request);
         }
